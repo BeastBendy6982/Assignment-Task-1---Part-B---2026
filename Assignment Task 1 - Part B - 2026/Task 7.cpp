d7e40c24401ea42bfa053966058c36bb1afbad9c
BeastBendy6982 <tobiasvanblommestein@gmail.com>
2026-03-13 15:57:29 +1100

    Update 8
    Changed all of task 7 so it completely works, and added comments for each of the parts. Updated take 8 to somewhat follow the requirements for it, but uses a regular bubble sort and linear search. There is more detail about the reason in the comments at the top of task 8.

--- Assignment Task 1 - Part B - 2026/Task 7.cpp
+++ Assignment Task 1 - Part B - 2026/Task 7.cpp
@@ -1,35 +1,22 @@
 #include <iostream>
 #include <string>
-#include <map>
 #include <cassert>
 #include <vector>
+#include <utility>
+#include <algorithm>
 using namespace std;
-void sort(int myArray[], int size);
 
 int main() {
-	int myArray[]{ 67, 13, 3, 89, 43, 2, 19, 71, 5, 61, 97, 7, 37, 31, 17, 11, 83, 53, 23, 29 }; // This has all the numbers in the array, and can be in whatever order
-	int size = sizeof(myArray) / sizeof(myArray[0]); // This divides the array of numbers to figure out how many different digits there are
+    // Below is the vector that is used to set pair to have two integers and then define pairs with the numbers contained in the curly brackets
+    vector<pair<int, int>> pairs = { {67, 13}, {3, 89}, {43, 2}, {19, 71}, {5, 61}, {97, 7}, {37, 31}, {17, 11}, {83, 53}, {23, 29} };
 
-	sort(myArray, size); // This is used to have sort defined more simply than at the top
+    // This is what actually sorts the pairs of integers, and sorts them in ascending order by default
+    sort(pairs.begin(), pairs.end());
 
-	for (int element : myArray) { // This is the loop that constantly gives the output for the next number
-		std::cout << element << " "; // This is how the output is formatted, with a space right after it to make each digit easier to read
-	}
+    // This is the loop that is used to print each of the pairs once they're sorted
+    for (const auto& p : pairs) { // What const auto& does is constantly and automatically convert auto into an int (since that's what's needed) and shortens pairs down to p
+        cout << "(" << p.first << ", " << p.second << ") "; // Each of the pairs printed are contained in brackets to make it easier to tell which numbers are paired together
+    }
 
-	return 0;
-}
-
-void sort(int myArray[], int size) { // This is what sort uses to actually sort all the numbers in the array
-
-	int storage; // Temporary storage to briefly store a number when moving it
-	for (int i = 0; i < size - 1; i++) { // Used as the base for defining how many digits are in the array
-		for (int j = 0; j < size - i - 1; j++) { // Used to constantly loop the way it checks if the current number is smaller or bigger than the next
-			// You can change the arrow's direction in if statement below between < (sort in descending order) or > (sort in ascending order) to make the numbers be sorted in that way
-			if (myArray[j] < myArray[j + 1]) { // Used to actually check if the current number is smaller or larger than the next number.
-				storage = myArray[j]; // Used as temporary storage for when the current number is smaller or larger than the next number
-				myArray[j] = myArray[j + 1]; // Used to increase the place in the array that the number is
-				myArray[j + 1] = storage; // Used to increase the number's place in the storage so it knows what place the number is once it finds a number that is smaller than it
-			}
-		}
-	}
+    return 0;
 }
--- Assignment Task 1 - Part B - 2026/Task 8.cpp
+++ Assignment Task 1 - Part B - 2026/Task 8.cpp
@@ -5,4 +5,55 @@
 #include <vector>
 using namespace std;
 
-// Task 8 needs the code from both task 7 and task 6 in order to be completed
+// ATTENTION: I am aware that this is a combo of the code from tasks 3 and 6, but this was as best I could do with the amount of time I had
+// I did have a go at making a binary searcher with the current bubble sort, but it very quickly became very difficult to get everything working
+// And just to clarify, it's a linear search with a regular bubble sorter instead of a binary search with a pairs bubble sort
+
+void sort(int arr[], int size);
+
+int findElement(int arr[], int n, int key) { // Used as the base to search for if the input is in the array
+	int i;
+	for (i = 0; i < n; i++) // The continuous loop to check the entire array for the input
+		if (arr[i] == key) // This is continued if the input was found in the array
+			return i;
+	return -1; // If the input wasn't found, this is continued instead
+}
+
+int main() {
+	int arr[]{ 67, 13, 3, 89, 43, 2, 19, 71, 5, 61, 97, 7, 37, 31, 17, 11, 83, 53, 23, 29 }; // This has all the numbers in the array, and can be in whatever order
+	int n = sizeof(arr) / sizeof(arr[0]); // This divides the array of numbers to figure out how many different digits there are
+	int key;
+
+	sort(arr, n); // This is used to have sort defined more simply than at the top
+	cout << "Input the number you want to search for in the array: ";
+	cin >> key;
+
+	int position = findElement(arr, n, key); // Defines what position is by searching with findElement
+	// The way it defines position is by getting the array itself, getting the amount of digits in the array, and searching for the input in the array and getting its place if found
+
+	// These are to make sure that the numbers 11, 23, and 97 have the correct value for their position in the array (spot 0 for 67, spot 2 for 3, and spot 11 for 7)
+	assert(position != arr[4]);
+	assert(position != arr[8]);
+	assert(position != arr[19]);
+
+	if (position == -1) // The if statement for the position, and uses what result findElement gives out depending on if the input was found
+		cout << "Input not found / found at position: " << position;
+	else
+		cout << "Input found at position: " << position;
+
+	return 0;
+}
+
+void sort(int arr[], int n) { // This is what sort uses to actually sort all the numbers in the array
+
+	int storage; // Temporary storage to briefly store a number when moving it
+	for (int i = 0; i < n - 1; i++) { // Used as the base for defining how many digits are in the array
+		for (int j = 0; j < n - i - 1; j++) { // Used to constantly loop the way it checks if the current number is smaller or bigger than the next
+			if (arr[j] > arr[j + 1]) { // Used to actually check if the current number is larger than the next number.
+				storage = arr[j]; // Used as temporary storage for when the current number is larger than the next number
+				arr[j] = arr[j + 1]; // Used to increase the place in the array that the number is
+				arr[j + 1] = storage; // Used to increase the number's place in the storage so it knows what place the number is once it finds a number that is smaller than it
+			}
+		}
+	}
+}
